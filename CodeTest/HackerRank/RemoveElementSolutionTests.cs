483a48bb4aff693e15ad6405332e772d3c9ee50f
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-01-27 10:26:55 +0700

    new: leet code problem 27: remove element (#3)

---
+++ CodeTest/HackerRank/RemoveElementSolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using HackerRank.Solutions;
+using Xunit;
+
+namespace CodeTest.HackerRank
+{
+    public class RemoveElementSolutionTests
+    {
+        [Theory]
+        [InlineData("1 2 2 3 4 5", 2, 4, "1 3 4 5")]
+        public void RemoveElementTest(string numsArray, int val, int actualLength, string expectedNumsArray)
+        {
+            var nums = numsArray.Split().Select(c => Convert.ToInt32(c)).ToList();
+            var expectedNums = expectedNumsArray.Split().Select(c => Convert.ToInt32(c)).ToList();
+            var removeElementSolution = new RemoveElementSolution();
+            var k = removeElementSolution.RemoveElement(nums, val);
+            Assert.Equal(actualLength, k);
+            for (var i = 0; i < actualLength; i++)
+            {
+                Assert.Equal(expectedNums[i], nums[i]);
+            }
+        }
+    }
+}
---
+++ HackerRank.Solutions/RemoveElementSolution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank.Solutions
+{
+    public class RemoveElementSolution
+    {
+        public int RemoveElement(List<int> nums, int val)
+        {
+            var length = 0;
+            for (var i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == val)
+                {
+                    continue;
+                }
+
+                nums[length] = nums[i];
+                length++;
+            }
+
+            return length;
+        }
+    }
+}
