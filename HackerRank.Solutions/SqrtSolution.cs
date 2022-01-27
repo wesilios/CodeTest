25f545bcbacbfcf8ea2d406b41efdf35a8bf4798
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-01-27 15:23:15 +0700

    add: leet code problem 69 sqrt of integer (#6)

---
+++ HackerRank.Solutions/SqrtSolution.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.Solutions
+{
+    public class SqrtSolution
+    {
+        public int Sqrt(int x)
+        {
+            long low = 0;
+            long high = x;
+            while (low + 1 < high)
+            {
+                var mid = low + (high - low) / 2;
+                if (mid * mid == x) return (int) mid;
+                if (mid * mid > x)
+                {
+                    high = mid;
+                    continue;
+                }
+
+                low = mid;
+            }
+
+            return high * high == x ? (int)high : (int)low;
+        }
+    }
+}
