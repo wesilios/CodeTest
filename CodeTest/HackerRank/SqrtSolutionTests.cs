301322d34576563d2d1d86402933e43aaf3d9a99
Que Nguyen <quenguyen10190@gmail.com>
2022-01-27 14:58:26 +0700

    add: leet code problem 69 sqrt of integer

---
+++ CodeTest/HackerRank/SqrtSolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using HackerRank.Solutions;
+using Xunit;
+
+namespace CodeTest.HackerRank
+{
+    public class SqrtSolutionTests
+    {
+        [Theory]
+        [InlineData(8, 2)]
+        [InlineData(2, 1)]
+        public void SqrtTest(int x, int expectedResult)
+        {
+            var sqrtSolution = new SqrtSolution();
+            Assert.Equal(expectedResult, sqrtSolution.Sqrt(x));
+        }
+    }
+}
