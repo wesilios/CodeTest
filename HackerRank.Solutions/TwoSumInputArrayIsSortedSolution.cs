dd4ffe5aa9500f71a29171a60ac64d6cfbf8ba0b
Que Nguyen <quenguyen10190@gmail.com>
2022-01-31 17:15:47 +0700

    add: leet code 167 problem: two sum with sorted array return array of indices.

---
+++ HackerRank.Solutions/TwoSumInputArrayIsSortedSolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.Solutions
+{
+    public class TwoSumInputArrayIsSortedSolution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var numsMap = new Dictionary<int, int>();
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var remain = target - numbers[i];
+                if (numsMap.ContainsKey(remain))
+                {
+                    return new[] { numsMap[remain] + 1, i + 1};
+                }
+
+                numsMap.TryAdd(numbers[i], i);
+            }
+            return Array.Empty<int>();
+        }
+    }
+}
