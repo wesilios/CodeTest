3451502fc5111f7f3d59900722cf3333faa4bbb3
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-01 16:15:48 +0700

    add: leet code 1002 find commons characters in list of words (#10)

---
+++ LeetCode.Solutions/CommonCharactersSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Solutions
+{
+    public class CommonCharactersSolution
+    {
+        public IList<string> CommonChars(string[] words)
+        {
+            const int asciiLength = 26;
+            var wordsLength = words.Length;
+
+            var dictionary = new int[asciiLength];
+
+            for (var i = 0; i < wordsLength; i++)
+            {
+                var word = words[i];
+
+                var tempDictionary = i == 0 ? dictionary : new int[asciiLength];
+
+                foreach (var ctx in word)
+                {
+                    tempDictionary[ctx - 'a']++;
+                }
+
+                for (var j = 0; j < asciiLength; j++)
+                {
+                    dictionary[j] = Math.Min(tempDictionary[j], dictionary[j]);
+                }
+            }
+
+            var commonChars = new List<string>();
+
+            for (var i = 0; i < dictionary.Length; i++)
+            {
+                var current = (char)(i + 'a');
+                var times = dictionary[i];
+                while (times-- > 0)
+                {
+                    commonChars.Add(current.ToString());
+                }
+            }
+
+            return commonChars;
+        }
+    }
+}
