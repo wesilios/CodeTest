6932444455cee1c386604cfef4d5822f13adea97
Que Nguyen <quenguyen10190@gmail.com>
2022-02-01 16:15:29 +0700

    add: leet code 1002 find commons characters in list of words

---
+++ CodeTest/LeetCode/CommonCharactersSolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class CommonCharactersSolutionTests
+    {
+        [Theory]
+        [InlineData("bella label roller", "e l l")]
+        [InlineData("cool lock cook", "c o")]
+        public void CommonCharsTest(string wordsString, string expectedResult)
+        {
+            var words = wordsString.Split();
+            var expected = expectedResult.Split();
+
+            var commonCharactersSolution = new CommonCharactersSolution();
+
+            var result = commonCharactersSolution.CommonChars(words);
+
+            Assert.Equal(expected.Length, result.Count);
+
+            foreach (var i in expected)
+            {
+                Assert.Contains(i, result);
+            }
+        }
+    }
+}
