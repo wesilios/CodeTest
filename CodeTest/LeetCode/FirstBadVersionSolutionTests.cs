78642a6d2e1f0196903562433c8601d47604f3ef
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-01 21:36:28 +0700

    add: leet code 278 first bad version using binary search with O(logn) (#11)

---
+++ CodeTest/LeetCode/FirstBadVersionSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class FirstBadVersionSolutionTests
+    {
+        [Theory]
+        [InlineData(1000000, 5467, 5467)]
+        [InlineData(1000000, 1, 1)]
+        [InlineData(5, 4, 4)]
+        [InlineData(2126753390, 1702766719, 1702766719)]
+        [InlineData(1, 1, 1)]
+        [InlineData(2, 2, 2)]
+        public void FirstBadVersionTesT(int versions, int badVersion, int expected)
+        {
+            var firstBadVersionSolution = new FirstBadVersionSolution(badVersion);
+
+            var result = firstBadVersionSolution.FirstBadVersion(versions);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCode.Solutions/FirstBadVersionSolution.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Solutions
+{
+    public class FirstBadVersionSolution : VersionControl
+    {
+        public FirstBadVersionSolution(int firstBadVersion) : base(firstBadVersion)
+        {
+        }
+
+        public int FirstBadVersion(int versions)
+        {
+            var start = 0;
+            var end = versions;
+
+            while (end - start > 1)
+            {
+                var middle = (end - start) / 2 + start;
+                if (IsBadVersion(middle))
+                {
+                    end = middle;
+                    continue;
+                }
+
+                start = middle;
+            }
+
+            return end;
+        }
+    }
+
+    public class VersionControl
+    {
+        private readonly int _firstBadVersion;
+
+        protected VersionControl(int firstBadVersion)
+        {
+            _firstBadVersion = firstBadVersion;
+        }
+
+        protected bool IsBadVersion(int version)
+        {
+            return version >= _firstBadVersion;
+        }
+    }
+}
