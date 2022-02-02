720bcd246e3c6ff63f1c261839e1135a033111f6
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-02 09:29:28 +0700

    add: leet code 704 binary search (#12)

---
+++ CodeTest/LeetCode/BinarySearchSolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class BinarySearchSolutionTests
+    {
+        [Theory]
+        [InlineData("-1 0 3 5 9 12", 9, 4)]
+        [InlineData("-1 0 3 5 9 12", 2, -1)]
+        [InlineData("5", 5, 0)]
+        [InlineData("1 5", 5, 1)]
+        [InlineData("5", -5, -1)]
+        public void SearchTest(string numbersArray, int target, int expected)
+        {
+            var numbers = numbersArray.Split().Select(c => Convert.ToInt32(c)).ToArray();
+
+            var binarySearchSolution = new BinarySearchSolution();
+
+            var result = binarySearchSolution.Search(numbers, target);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCode.Solutions/BinarySearchSolution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Solutions
+{
+    public class BinarySearchSolution
+    {
+        public int Search(int[] numbers, int target)
+        {
+            var start = 0;
+            var end = numbers.Length - 1;
+            const int notFound = -1;
+
+            while (start <= end)
+            {
+                var middle = (end + start) / 2;
+                var value = numbers[middle];
+                if (value == target) return middle;
+                if (value < target)
+                {
+                    start = middle + 1;
+                    continue;
+                }
+
+                end = middle - 1;
+            }
+
+            return notFound;
+        }
+    }
+}
