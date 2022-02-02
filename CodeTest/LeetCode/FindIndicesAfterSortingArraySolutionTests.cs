742f05f672daa52d3114ddedaa7bf2d2ae747fdd
Que Nguyen <quenguyen10190@gmail.com>
2022-02-02 10:19:37 +0700

    add: leet code 2089 find indices after sorting array
    update: change library owner of IntersectionOfTwoArraysSolution and its tests

---
+++ CodeTest/LeetCode/FindIndicesAfterSortingArraySolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class FindIndicesAfterSortingArraySolutionTests
+    {
+        [Theory]
+        [InlineData("1 2 5 2 3", 2, 2)]
+        [InlineData("1 2 5 2 2", 2, 3)]
+        [InlineData("1 2 5 2 3", 4, 0)]
+        public void TargetIndicesTest(string numberArray, int target, int expectedLength)
+        {
+            var numbers = numberArray.Split().Select(c => Convert.ToInt32(c)).ToArray();
+
+            var findIndicesAfterSortingArraySolution = new FindIndicesAfterSortingArraySolution();
+
+            var indices = findIndicesAfterSortingArraySolution.TargetIndices(numbers, target);
+            
+            Assert.Equal(expectedLength, indices.Count);
+            
+            foreach (var ctx in indices)
+            {
+                Assert.Equal(target, numbers[ctx]);
+            }
+        }
+    }
+}
--- CodeTest/LeetCode/IntersectionOfTwoArraysSolutionTests.cs
+++ CodeTest/LeetCode/IntersectionOfTwoArraysSolutionTests.cs
@@ -1,8 +1,8 @@
 ﻿using System.Linq;
-using HackerRank.Solutions;
+using LeetCode.Solutions;
 using Xunit;
 
-namespace CodeTest.HackerRank
+namespace CodeTest.LeetCode
 {
     public class IntersectionOfTwoArraysSolutionTests
     {
