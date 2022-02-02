443930087f8261f373ed37900c9056bb6ce599f4
Que Nguyen <quenguyen10190@gmail.com>
2022-02-02 17:35:27 +0700

    add: leet code 374 guess number higher or lower

---
+++ CodeTest/LeetCode/GuessNumberHigherOrLowerSolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class GuessNumberHigherOrLowerSolutionTests
+    {
+        [Theory]
+        [InlineData(10, 6, 6)]
+        [InlineData(2126753390, 1702766719, 1702766719)]
+        public void GuessNumberTest(int n, int pick, int expected)
+        {
+            var guessNumberHigherOrLowerSolution = new GuessNumberHigherOrLowerSolution(pick);
+
+            var guess = guessNumberHigherOrLowerSolution.GuessNumber(n);
+            
+            Assert.Equal(expected, guess);
+        }
+    }
+}
---
+++ LeetCode.Solutions/GuessNumberHigherOrLowerSolution.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.Solutions
+{
+    public class GuessNumberHigherOrLowerSolution : GuessGame
+    {
+        public GuessNumberHigherOrLowerSolution(int pick) : base(pick)
+        {
+        }
+
+        public int GuessNumber(int n)
+        {
+            var start = 1;
+            var end = int.MaxValue;
+            var guess = n;
+            var guessResult = Guess(guess);
+
+            while (guessResult != 0)
+            {
+                if (guessResult == -1)
+                {
+                    end = guess - 1;
+                }
+                else
+                {
+                    start = guess + 1;
+                }
+
+                guess = start + (end - start) / 2;
+                guessResult = Guess(guess);
+            }
+
+            return guess;
+        }
+    }
+
+    public class GuessGame
+    {
+        private readonly int _pick;
+
+        protected GuessGame(int pick)
+        {
+            _pick = pick;
+        }
+
+        protected int Guess(int n)
+        {
+            if (_pick < n) return -1;
+            if (_pick > n) return 1;
+            return 0;
+        }
+    }
+}
