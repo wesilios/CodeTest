d7880db3b70c75772a44be684f2bdc7b78ce2e5b
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-02 10:21:37 +0700

    add: leet code 2089 find indices after sorting array (#13)
    update: change library owner of IntersectionOfTwoArraysSolution and its tests

---
+++ LeetCode.Solutions/FindIndicesAfterSortingArraySolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Solutions
+{
+    public class FindIndicesAfterSortingArraySolution
+    {
+        public IList<int> TargetIndices(int[] nums, int target)
+        {
+            Array.Sort(nums);
+
+            var result = new List<int>();
+
+            var start = 0;
+            var end = nums.Length - 1;
+
+            while (start <= end)
+            {
+                if (nums[start] == target && nums[end] == target) break;
+                if (nums[start] != target) start++;
+                if (nums[end] != target) end--;
+            }
+
+            for (var i = start; i <= end; i++)
+            {
+                result.Add(i);
+            }
+
+            return result;
+        }
+    }
+}
