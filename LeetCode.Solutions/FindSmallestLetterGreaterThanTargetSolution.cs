ea6d8ae440fc82abc8e66e20de303063a13b0b2f
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-03 23:21:46 +0700

    add: leet code 744 find smallest letter greater than target letter (#18)

---
+++ LeetCode.Solutions/FindSmallestLetterGreaterThanTargetSolution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Solutions
+{
+    public class FindSmallestLetterGreaterThanTargetSolution
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            var low = 0;
+            var high = letters.Length - 1;
+            
+            while (low <= high)
+            {
+                var middle = low + (high - low) / 2;
+                if (letters[middle] > target)
+                {
+                    high = middle - 1;
+                    continue;
+                }
+
+                low = middle + 1;
+            }
+
+            return letters[low % letters.Length];
+        }
+    }
+}
