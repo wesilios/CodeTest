a8c241ead9ed1846c446150e57855e7512f9b3b0
Que Nguyen <quenguyen10190@gmail.com>
2022-02-03 15:31:24 +0700

    add: leet code longest palindromic substring

---
+++ CodeTest/LeetCode/LongestPalindromicSubstringSolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using HackerRank.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class LongestPalindromicSubstringSolutionTests
+    {
+        [Theory]
+        [InlineData("babad", "aba bab")]
+        [InlineData("babbab", "babbab")]
+        [InlineData("aaab", "aaa")]
+        [InlineData("baa", "aa")]
+        [InlineData("aaa", "aaa")]
+        [InlineData("lfcwnnwcwfl", "cwnnwc")]
+        [InlineData("a", "a")]
+        [InlineData("ac", "a c")]
+        [InlineData("eabcb", "bcb")]
+        public void LongestPalindromeTest(string s, string expectedResults)
+        {
+            var longestPalindromicSubstringSolution = new LongestPalindromicSubstringSolution();
+
+            var result = longestPalindromicSubstringSolution.LongestPalindrome(s);
+
+            var expected = expectedResults.Split();
+            
+            Assert.Contains(result, expected);
+        }
+    }
+}
---
+++ HackerRank.Solutions/LongestPalindromicSubstringSolution.cs
@@ -0,0 +1,38 @@
+﻿namespace HackerRank.Solutions
+{
+    public class LongestPalindromicSubstringSolution
+    {
+        public string LongestPalindrome(string str)
+        {
+            var strLength = str.Length;
+            var memoization = new bool[strLength, strLength];
+            var li = -1;
+            var lj = -1;
+
+            for (var d = 0; d < strLength; d++)
+            {
+                for (int i = 0, j = d; j < strLength; i++, j++)
+                {
+                    if (d == 0)
+                    {
+                        memoization[i, j] = true;
+                    }
+                    else if (d == 1)
+                    {
+                        memoization[i, j] = str[i] == str[j];
+                    }
+                    else
+                    {
+                        memoization[i, j] = str[i] == str[j] && memoization[i + 1, j - 1];
+                    }
+
+                    if (!memoization[i, j]) continue;
+                    li = i;
+                    lj = j;
+                }
+            }
+
+            return str.Substring(li, lj - li + 1);
+        }
+    }
+}
