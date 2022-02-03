52973172fbe2e4babdf308465cfa5efbb8840d02
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-03 16:30:50 +0700

    add: leet code 125 check valid palindrome string (#17)
    * add: leet code 125 check valid palindrome string
    
    todo: add more unit test for string helper
    
    * update: add more unit test for manipulating string before check valid palindrome

---
+++ CodeTest/LeetCode/ValidPalindromeSolutionTests.cs
@@ -0,0 +1,54 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class ValidPalindromeSolutionTests
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        public void IsValidPalindrome(string s, bool expected)
+        {
+            var validPalindromeSolution = new ValidPalindromeSolution();
+
+            Assert.Equal(expected, validPalindromeSolution.IsValidPalindrome(s));
+        }
+
+        [Theory]
+        [InlineData("", " ", "")]
+        [InlineData("This is a sentence with multiple    spaces", " ", "This is a sentence with multiple spaces")]
+        [InlineData("This is a sentence with multiple    spaces", "", "This is a sentence with multiplespaces")]
+        [InlineData("This is     a sentence      with multiple    spaces in multiple places", " ",
+            "This is a sentence with multiple spaces in multiple places")]
+        [InlineData("This is     a sentence      with multiple    spaces in multiple places", "",
+            "This isa sentencewith multiplespaces in multiple places")]
+        [InlineData("This is a sentence with multiple spaces", " ", "This is a sentence with multiple spaces")]
+        [InlineData("This is a sentence with multiple spaces", "", "This is a sentence with multiple spaces")]
+        public void TrimDuplicateSpacesBetweenCharacters_SuccessCases(string input, string replacement,
+            string expectResult)
+        {
+            var validPalindromeSolution = new ValidPalindromeSolution();
+
+            var result = validPalindromeSolution.TrimDuplicateSpacesBetweenCharacters(input, replacement);
+
+            Assert.True(string.Equals(result, expectResult));
+        }
+
+        [Theory]
+        [InlineData("", " ", "")]
+        [InlineData("This is !@#$%^&*()?><-,_+=`~[];':\"/\\a sentence", " ", "This is a sentence")]
+        [InlineData("This is !@#$%^&*()?><-,_+=`~[];':\"/\\a sentence", "", "Thisisasentence")]
+        [InlineData("!@#$%^&*()?><-,_+=`~[];':\"/\\", " ", " ")]
+        [InlineData("!@#$%^&*()?><-,_+=`~[];':\"/\\", "", "")]
+        public void TrimSpecialCharacters_SuccessCases(string input, string replacement, string expectResult)
+        {
+            var validPalindromeSolution = new ValidPalindromeSolution();
+
+            var result = validPalindromeSolution.TrimSpecialCharacters(input, replacement);
+
+            Assert.True(string.Equals(result, expectResult));
+        }
+    }
+}
---
+++ LeetCode.Solutions/ValidPalindromeSolution.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode.Solutions
+{
+    public class ValidPalindromeSolution
+    {
+        public bool IsValidPalindrome(string s)
+        {
+            var str = TrimDuplicateSpacesBetweenCharacters(s, "");
+            str = TrimSpecialCharacters(str, "");
+            str = str.Trim();
+            str = str.ToLowerInvariant();
+
+            var start = 0;
+            var end = str.Length - 1;
+            while (end > start)
+            {
+                if (str[start++] != str[end--]) return false;
+            }
+
+            return true;
+        }
+
+        public string TrimSpecialCharacters(string str, string replacement = " ")
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+
+            var regEx = new Regex("[^A-Za-z0-9]");
+            return Regex.Replace(regEx.Replace(str, replacement), @"\s+", replacement);
+        }
+
+        public string TrimDuplicateSpacesBetweenCharacters(string str, string replacement = " ")
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+
+            var regex = new Regex("[ ]{2,}", RegexOptions.None);
+            str = regex.Replace(str, replacement);
+
+            return str;
+        }
+    }
+}
