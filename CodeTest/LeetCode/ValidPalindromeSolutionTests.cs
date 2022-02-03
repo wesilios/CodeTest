aa3e1179c187c612d1e323e67184037c9f2c43a3
Que Nguyen <quenguyen10190@gmail.com>
2022-02-03 16:06:07 +0700

    add: leet code 125 check valid palindrome string
    todo: add more unit test for string helper

---
+++ CodeTest/LeetCode/ValidPalindromeSolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class ValidPalindromeSolutionTests
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        public void IsValidPalindrome(string s, bool expected)
+        {
+            var validPalindromeSolution = new ValidPalindromeSolution();
+
+            Assert.Equal(expected, validPalindromeSolution.IsValidPalindrome(s));
+        }
+    }
+}
---
+++ LeetCode.Solutions/ValidPalindromeSolution.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode.Solutions
+{
+    public class ValidPalindromeSolution
+    {
+        public bool IsValidPalindrome(string s)
+        {
+            var str = TrimSpecialCharacters(s, "");
+            str = TrimDuplicateSpacesBetweenCharacters(str, "");
+            str = TrimSpaces(str, "");
+            str = str.Trim();
+            str = str.ToLowerInvariant();
+
+            var start = 0;
+            var end = str.Length - 1;
+            while (end > start)
+            {
+                if (str[start++] != str[end--]) return false;
+            }
+
+            return true;
+        }
+
+        public string TrimSpecialCharacters(string str, string replacement = " ")
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+
+            var regEx = new Regex("[^A-Za-z0-9]");
+            return Regex.Replace(regEx.Replace(str, replacement), @"\s+", replacement);
+        }
+
+        public string TrimDuplicateSpacesBetweenCharacters(string str, string replacement = " ")
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+
+            var regex = new Regex("[ ]{2,}", RegexOptions.None);
+            str = regex.Replace(str, replacement);
+
+            return str;
+        }
+
+        public string TrimSpaces(string str, string replacement = " ")
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+
+            var regex = new Regex("[ ]{1,}", RegexOptions.None);
+            str = regex.Replace(str, replacement);
+
+            return str;
+        }
+    }
+}
