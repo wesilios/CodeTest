99405e966bddf73e659aab01ee5b4dc451c29470
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-07 22:06:42 +0700

    add: leet code 141 linked list cycle (#22)

---
+++ CodeTest/LeetCode/LinkedListCycleSolutionTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using CodeTest.Library;
+using HackerRank.Solutions;
+using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class LinkedListCycleSolutionTests
+    {
+        [Theory]
+        [InlineData("3 2 0 -4", 0, true)]
+        [InlineData("3 2 0 -4", 1, true)]
+        [InlineData("3 2 0 -4", -1, false)]
+        [InlineData("1", -1, false)]
+        [InlineData("", -1, false)]
+        public void HasCycleTest(string listData, int pos, bool expected)
+        {
+            var linkedListSolution = new LinkedListSolution();
+            var inputData = listData.Split();
+            var head = inputData.Aggregate<string, SinglyLinkedListNode<string>>(null,
+                linkedListSolution.InsertNodeToTail);
+
+            var tail = head;
+
+            while (tail.Next != null)
+            {
+                tail = tail.Next;
+            }
+
+            var tempPos = 0;
+
+            var node = head;
+            while (tempPos <= pos)
+            {
+                if (tempPos == pos)
+                {
+                    tail.Next = node;
+                    break;
+                }
+
+                tempPos++;
+                node = node.Next;
+            }
+
+            var linkedListCycleSolution = new LinkedListCycleSolution();
+
+            var result = linkedListCycleSolution.HasCycle(head);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCode.Solutions/LinkedListCycleSolution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using CodeTest.Library;
+
+namespace LeetCode.Solutions
+{
+    public class LinkedListCycleSolution
+    {
+        public bool HasCycle<T>(SinglyLinkedListNode<T> head)
+        {
+            if (head == null) return false;
+            
+            var node = head;
+
+            var nodeDictionary = new Dictionary<SinglyLinkedListNode<T>, SinglyLinkedListNode<T>>();
+
+            var result = false;
+
+            while (node.Next != null)
+            {
+                if (nodeDictionary.ContainsKey(node.Next))
+                {
+                    result = true;
+                    break;
+                }
+                
+                nodeDictionary.Add(node, node);
+                node = node.Next;
+            }
+
+            return result;
+        }
+    }
+}
