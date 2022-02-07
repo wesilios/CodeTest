71d479d9b8ca5cf11a23f0df8cea70aee29088b5
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-07 16:36:25 +0700

    add: leet code 136 single number (#21)

---
+++ LeetCode.Solutions/SingleNumberSolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Solutions
+{
+    public class SingleNumberSolution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var numsMap = new HashSet<int>();
+
+            foreach (var num in nums)
+            {
+                if (numsMap.Contains(num))
+                {
+                    numsMap.Remove(num);
+                    continue;
+                }
+                
+                numsMap.Add(num);
+            }
+
+            return numsMap.First();
+        }
+    }
+}
