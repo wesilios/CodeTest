d4a295a7336c716e85ef2561f0853e4842aae41c
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-07 16:19:14 +0700

    add: leet code 118 pascal triangle (#20)

---
+++ CodeTest/LeetCode/PascalTriangleSolutionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class PascalTriangleSolutionTests
+    {
+        [Theory]
+        [InlineData(5, "1 1-1 1-2-1 1-3-3-1 1-4-6-4-1")]
+        [InlineData(2, "1 1-1")]
+        [InlineData(1, "1")]
+        public void GenerateTest(int n, string expectedResult)
+        {
+            var expected = expectedResult.Split().Select(c => c.Split('-').Select(x => Convert.ToInt32(x)).ToList())
+                .ToList();
+
+            var pascalTriangleSolution = new PascalTriangleSolution();
+
+            var result = pascalTriangleSolution.Generate(n);
+
+            for (var index = 0; index < expected.Count; index++)
+            {
+                var row = expected[index];
+                for (var i = 0; i < row.Count; i++)
+                {
+                    Assert.Equal(expected[index][i], result[index][i]);
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode.Solutions/PascalTriangleSolution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions
+{
+    public class PascalTriangleSolution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> result = new List<IList<int>>
+            {
+                new List<int> { 1 }
+            };
+
+            for (var i = 1; i < numRows; i++)
+            {
+                var row = new List<int>();
+                for (var j = 0; j <= i; j++)
+                {
+                    if (j == 0 || j == i)
+                    {
+                        row.Add(result[i - 1][0]);
+                        continue;
+                    }
+
+                    row.Add(result[i - 1][j-1] + result[i - 1][j]);
+                }
+
+                result.Add(row);
+            }
+
+            return result;
+        }
+    }
+}
