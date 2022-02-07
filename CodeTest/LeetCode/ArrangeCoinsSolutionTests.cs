024b83044e1001d117fce4bde3158a4fab233884
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-07 14:25:44 +0700

    add: leet code 441 arrange coins (#19)

---
+++ CodeTest/LeetCode/ArrangeCoinsSolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class ArrangeCoinsSolutionTests
+    {
+        [Theory]
+        [InlineData(5, 2)]
+        [InlineData(8, 3)]
+        [InlineData(2126753390, 65218)]
+        public void ArrangeCoinsTest(int coins, int expected)
+        {
+            var arrangeCoinsSolution = new ArrangeCoinsSolution();
+
+            Assert.Equal(expected, arrangeCoinsSolution.ArrangeCoins(coins));
+        }
+    }
+}
---
+++ LeetCode.Solutions/ArrangeCoinsSolution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions
+{
+    public class ArrangeCoinsSolution
+    {
+        public int ArrangeCoins(int n)
+        {
+            var row = 1;
+            while (true)
+            {
+                var remain = n - row;
+                if (remain < row + 1)
+                {
+                    break;
+                }
+
+                n = remain;
+                row++;
+            }
+
+            return row;
+        }
+    }
+}
