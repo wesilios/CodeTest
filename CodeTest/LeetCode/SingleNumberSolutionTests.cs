148fa8c507210291cdfeaa0e11cc0fa07daa9708
Que Nguyen <quenguyen10190@gmail.com>
2022-02-07 16:36:07 +0700

    add: leet code 136 single number

---
+++ CodeTest/LeetCode/SingleNumberSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class SingleNumberSolutionTests
+    {
+        [Theory]
+        [InlineData("2 2 1", 1)]
+        [InlineData("4 1 2 1 2", 4)]
+        public void SingleNumber(string inputs, int expected)
+        {
+            var nums = inputs.Split().Select(c => Convert.ToInt32(c)).ToArray();
+            
+            var singleNumberSolution = new SingleNumberSolution();
+
+            var result = singleNumberSolution.SingleNumber(nums);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
