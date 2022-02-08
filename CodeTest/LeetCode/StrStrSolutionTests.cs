91c500783ef3af566a90223b1746df612a975d04
Que Nguyen <quenguyen10190@gmail.com>
2022-02-08 12:09:49 +0700

    add: leet code 28 implement strStr

---
+++ CodeTest/LeetCode/StrStrSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class StrStrSolutionTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("aaaaa", "bba")]
+        [InlineData("aaaaa", "a")]
+        [InlineData("hello", "ll")]
+        [InlineData("heeill", "ll")]
+        public void StrStrTest(string haystack, string needle)
+        {
+            var strStrSolution = new StrStrSolution();
+
+            var result = strStrSolution.StrStr(haystack, needle);
+
+            Assert.Equal(haystack.IndexOf(needle, StringComparison.InvariantCulture), result);
+        }
+    }
+}
---
+++ LeetCode.Solutions/StrStrSolution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Solutions
+{
+    public class StrStrSolution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) return 0;
+            var indexOf = -1;
+            if (haystack.Length < needle.Length) return indexOf;
+            var i = 0;
+            while (i + needle.Length <= haystack.Length)
+            {
+                var subString = haystack.Substring(i, needle.Length);
+                if (subString.Equals(needle))
+                {
+                    indexOf = i;
+                    break;
+                }
+
+                i++;
+            }
+
+            return indexOf;
+        }
+    }
+}
