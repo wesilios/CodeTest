8ca4d2219c6280e87c954de1e7ab673046c5d300
Que Nguyen <quenguyen10190@gmail.com>
2022-02-09 14:24:58 +0700

    add: leet code 70 climbing stars solution and unit test

---
+++ CodeTest/LeetCode/ClimbStairsSolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class ClimbStairsSolutionTests
+    {
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        public void ClimbStairsTest(int n, int expected)
+        {
+            var climbStairsSolution = new ClimbStairsSolution();
+
+            var result = climbStairsSolution.ClimbStairs(n);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
