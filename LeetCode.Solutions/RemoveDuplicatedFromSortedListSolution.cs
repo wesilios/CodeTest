a364bdbdbac5ffa0e4ea390e4b7d4e8c63f03c3b
Que Nguyen <quenguyen10190@gmail.com>
2022-02-09 15:13:29 +0700

    add: leet code 83 remove duplicated from sorted list

---
+++ LeetCode.Solutions/RemoveDuplicatedFromSortedListSolution.cs
@@ -0,0 +1,24 @@
+﻿using CodeTest.Library;
+
+namespace LeetCode.Solutions
+{
+    public class RemoveDuplicatedFromSortedListSolution
+    {
+        public SinglyLinkedListNode<int> DeleteDuplicates(SinglyLinkedListNode<int> head)
+        {
+            var node = head;
+            while (node != null)
+            {
+                if (node.Next == null) break;
+                if (node.Data.Equals(node.Next.Data))
+                {
+                    node.Next = node.Next.Next;
+                    continue;
+                }
+
+                node = node.Next;
+            }
+            return head;
+        }
+    }
+}
