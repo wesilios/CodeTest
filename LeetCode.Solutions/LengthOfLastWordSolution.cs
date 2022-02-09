7c1cc0fa3f6a1d44b64c061709dee3dc380a0133
Que Nguyen <quenguyen10190@gmail.com>
2022-02-09 14:36:11 +0700

    add: leet code 58 length of last word

---
+++ LeetCode.Solutions/LengthOfLastWordSolution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions
+{
+    public class LengthOfLastWordSolution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var stringArray = s.Split();
+
+            if (stringArray.Length == 0) return 0;
+
+            var lastWord = string.Empty;
+
+            for (var i = stringArray.Length - 1; i >= 0; i--)
+            {
+                if(string.IsNullOrEmpty(stringArray[i])) continue;
+                lastWord = stringArray[i];
+                break;
+            }
+
+            return lastWord.Length;
+        }
+    }
+}
