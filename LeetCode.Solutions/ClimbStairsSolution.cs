d312b89e83512de0ba543d06bf08fe681ead550e
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-09 15:13:53 +0700

    add: leet code 83 remove duplicated from sorted list (#26)

--- LeetCode.Solutions/ClimbStairsSolution.cs
+++ LeetCode.Solutions/ClimbStairsSolution.cs
@@ -1,10 +1,14 @@
-﻿namespace LeetCode.Solutions
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions
 {
     public class ClimbStairsSolution
     {
         public int ClimbStairs(int n)
         {
-            
+            var result = new List<int[]>();
+
+            return result.Count;
         }
     }
 }
