0fdd6d74c5e67bcf1ab3e1497001c1b9d907013a
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-09 14:42:37 +0700

    add: leet code 58 length of last word (#25)

---
+++ CodeTest/LeetCode/LengthOfLastWordSolutionTest.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class LengthOfLastWordSolutionTest
+    {
+        [Theory]
+        [InlineData("Hello World", 5)]
+        [InlineData("luffy is still joyboy", 6)]
+        [InlineData("   fly me   to   the moon  ", 4)]
+        [InlineData("", 0)]
+        public void LengthOfLastWord(string input, int expected)
+        {
+            var lengthOfLastWordSolution = new LengthOfLastWordSolution();
+
+            var result = lengthOfLastWordSolution.LengthOfLastWord(input);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
