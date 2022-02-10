77121d901163277ba4ee6e0f378dc04fca1552fb
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-02-10 15:59:54 +0700

    add: leet code 3 Longest Substring Without Repeating Characters (#27)

---
+++ CodeTest/LeetCode/LengthOfLongestSubstringSolutionTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Solutions;
+using Xunit;
+
+namespace CodeTest.LeetCode
+{
+    public class LengthOfLongestSubstringSolutionTest
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void GetLengthOfLongestSubstringTest(string s, int expectedResult)
+        {
+            var lengthOfLongestSubstringSolution = new LengthOfLongestSubstringSolution();
+            Assert.Equal(expectedResult, lengthOfLongestSubstringSolution.GetLengthOfLongestSubstring(s));
+        }
+    }
+}
