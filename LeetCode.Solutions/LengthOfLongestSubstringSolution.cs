b03ca8c37cfe5dd83aecb2bb388c9082c16ea70b
Que Nguyen <quenguyen10190@gmail.com>
2022-02-10 15:59:17 +0700

    add: leet code 3 Longest Substring Without Repeating Characters

---
+++ LeetCode.Solutions/LengthOfLongestSubstringSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Solutions
+{
+    public class LengthOfLongestSubstringSolution
+    {
+        public int GetLengthOfLongestSubstring(string s)
+        {
+            var chars = new int[256];
+            Array.Fill(chars, -1);
+
+            var left = 0;
+            var right = 0;
+            var result = 0;
+            while (right < s.Length)
+            {
+                var r = s[right];
+                var index = chars[r];
+                if (index != -1 && index >= left && index < right)
+                {
+                    left = index + 1;
+                }
+
+                result = Math.Max(result, right - left + 1);
+                chars[r] = right;
+                right++;
+            }
+
+            return result;
+        }
+    }
+}
