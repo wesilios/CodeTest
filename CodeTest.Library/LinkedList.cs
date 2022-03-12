ad33b081df07b09e0cb48995153be847cf4996b9
Que Thien Nguyen <quenguyen10190@gmail.com>
2022-03-12 16:51:35 +0700

    add: leet code 92 reverse linked list between range (#33)

---
+++ CodeTest.Library/LinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeTest.Library
+{
+    public class LinkedList
+    {
+        public SinglyLinkedListNode<int> MakeLinkedListFromTail(IEnumerable<string> stringArray)
+        {
+            return stringArray
+                .Select(c => Convert.ToInt32(c))
+                .Aggregate<int, SinglyLinkedListNode<int>>(null, InsertNodeToTail);
+        }
+
+        public SinglyLinkedListNode<T> InsertNodeToTail<T>(SinglyLinkedListNode<T> head, T data)
+        {
+            var node = new SinglyLinkedListNode<T>(data);
+            if (head == null) return node;
+            var temp = head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+
+            temp.Next = node;
+            return head;
+        }
+    }
+}
