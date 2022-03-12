ed3612f19d814cf21f177f9f6f095d0e62a7f581
Que Nguyen <quenguyen10190@gmail.com>
2022-03-12 16:50:29 +0700

    add: leet code 92 reverse linked list between range

---
+++ CodeTest/Libraries/LinkedListTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using CodeTest.Library;
+using Xunit;
+
+namespace CodeTest.Libraries
+{
+    public class LinkedListTest
+    {
+        [Theory]
+        [InlineData("1,2,3,4,5,6", "123456")]
+        public void InsertIntoTailTest(string input, string expectedResult)
+        {
+            var linkedList = new LinkedList();
+            var inputData = input.Split(",");
+            var head = inputData.Aggregate<string, SinglyLinkedListNode<string>>(null,
+                linkedList.InsertNodeToTail);
+
+            var result = string.Empty;
+            var temp = head;
+            while (temp != null)
+            {
+                result += temp.Data;
+                temp = temp.Next;
+            }
+
+            Assert.Equal(expectedResult, result);
+        }
+        
+        [Theory]
+        [InlineData("1,2,3,4,5,6", "123456")]
+        public void MakeLinkedListFromTailTest(string input, string expectedResult)
+        {
+            var linkedList = new LinkedList();
+            var inputData = input.Split(",");
+            var head = linkedList.MakeLinkedListFromTail(inputData);
+
+            var result = string.Empty;
+            var temp = head;
+            while (temp != null)
+            {
+                result += temp.Data;
+                temp = temp.Next;
+            }
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ LeetCode.Solutions/ReverseLinkedListSolution.cs
@@ -0,0 +1,61 @@
+﻿using CodeTest.Library;
+
+namespace LeetCode.Solutions
+{
+    public class ReverseLinkedListSolution
+    {
+        public SinglyLinkedListNode<T> ReverseLinkedList<T>(SinglyLinkedListNode<T> head)
+        {
+            if (head == null) return null;
+            var current = head;
+            SinglyLinkedListNode<T> prev = null;
+            while (current != null)
+            {
+                var next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+
+        public SinglyLinkedListNode<T> ReverseLinkedListInBetween<T>(SinglyLinkedListNode<T> head, int left, int right)
+        {
+            if (left == 0 || right == 0 || left >= right) return head;
+            var currentPosition = 1;
+            var currentNode = head;
+            SinglyLinkedListNode<T> prev = null;
+            while (currentPosition < left)
+            {
+                prev = currentNode;
+                currentNode = currentNode.Next;
+                currentPosition++;
+            }
+
+            var prevLeftNode = prev;
+            var leftNode = currentNode;
+            SinglyLinkedListNode<T> rightNode = null;
+            while (currentPosition <= right)
+            {
+                if (currentPosition == right)
+                {
+                    rightNode = currentNode;
+                    leftNode.Next = currentNode.Next;
+                }
+
+                var next = currentNode.Next;
+                currentNode.Next = prev;
+                prev = currentNode;
+                currentNode = next;
+
+                currentPosition++;
+            }
+
+            if (prevLeftNode == null) return rightNode;
+            prevLeftNode.Next = rightNode;
+            return head;
+
+        }
+    }
+}
