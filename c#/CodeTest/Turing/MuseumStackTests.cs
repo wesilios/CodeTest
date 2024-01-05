ace0756ab6c26428215765d9c5694199e33dc326
Que Thien Nguyen <quenguyen10190@gmail.com>
2024-01-05 07:22:13 +0700

    feat: solve the problem with dictionary (#40)

---
+++ c#/CodeTest/Turing/MuseumStackTests.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using Turing.Solutions;
+using Xunit;
+
+namespace CodeTest.Turing;
+
+public class MuseumStackTests
+{
+    [Theory]
+    [InlineData("1,2,3", 1)]
+    [InlineData("2,2,2,3,3", 2)]
+    [InlineData("2,2,2,3,3,5,5", 2)]
+    public void RunTest(string input, int expectedResult)
+    {
+        var k = input.Split(",").Select(int.Parse).ToArray();
+        var solution = new MuseumStack();
+
+        var result = solution.Solution(k);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ c#/Turing.Solutions/MuseumStack.cs
@@ -0,0 +1,36 @@
+﻿namespace Turing.Solutions;
+
+public class MuseumStack
+{
+    public int Solution(int[] k)
+    {
+        var dictionary = new Dictionary<int, int>();
+        foreach (var item in k)
+        {
+            if (dictionary.TryAdd(item, 1)) continue;
+            dictionary[item]++;
+        }
+
+        var count = -1;
+        foreach (var (key, value) in dictionary)
+        {
+            var temp = 0;
+            if (dictionary.TryGetValue(key - 1, out var before))
+            {
+                temp += before;
+            }
+
+            if (dictionary.TryGetValue(key + 1, out var after))
+            {
+                temp += after;
+            }
+
+            if (temp > 0 && (temp < count || count == -1))
+            {
+                count = temp;
+            }
+        }
+
+        return count;
+    }
+}
