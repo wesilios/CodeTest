898a4b6fa62af8573229d0ba69fe9574a83dcad7
Que Nguyen <quenguyen10190@gmail.com>
2024-01-13 21:42:47 +0700

    feat: add the implementation

--- c#/CodeTest/LeetCode/ThreeSumSolutionTests.cs
+++ c#/CodeTest/LeetCode/ThreeSumSolutionTests.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Solutions;
 using Xunit;
 
 namespace CodeTest.LeetCode;
@@ -5,13 +8,33 @@ namespace CodeTest.LeetCode;
 public class ThreeSumSolutionTests
 {
     [Theory]
-    [InlineData("", "")]
+    [InlineData("-1,0,1,2,-1,-4", "-1,-1,2|-1,0,1")]
+    [InlineData("0,1,1", "")]
+    [InlineData("0,0,0", "0,0,0")]
     public void ThreeSumTest(string input, string expectedResults)
     {
         //Arrange
+        var nums = input.Split(",").Select(int.Parse).ToArray();
+        var solution = new ThreeSumSolution();
+        var expectResultNums = new List<List<int>>();
+        if (!string.IsNullOrEmpty(expectedResults))
+        {
+            expectResultNums = expectedResults.Split("|").Select(x => x.Split(",").Select(int.Parse).ToList()).ToList();
+        }
 
         //Act
+        var results = solution.ThreeSum(nums);
 
         //Assert
+        var result = true;
+        // foreach (var expectedResult in expectedResults)
+        // {
+        //     foreach (var item in results)
+        //     {
+        //         
+        //     }
+        // }
+        
+        Assert.True(result);
     }
 }
--- c#/LeetCode.Solutions/ThreeSumSolution.cs
+++ c#/LeetCode.Solutions/ThreeSumSolution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Solutions
 {
@@ -7,7 +8,44 @@ namespace LeetCode.Solutions
     {
         public IList<IList<int>> ThreeSum(int[] nums)
         {
-            throw new NotImplementedException();
+            var triplets = new List<IList<int>>();
+            if (nums.Length < 3) return triplets;
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (nums[i] > 0) break;
+
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                var left = i + 1;
+                var right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    var sum = nums[i] + nums[left] + nums[right];
+
+                    switch (sum)
+                    {
+                        case 0:
+                        {
+                            triplets.Add(new List<int> { nums[i], nums[left], nums[right] });
+                            while (left < right && nums[left] == nums[left + 1]) left++;
+                            while (left < right && nums[right] == nums[right - 1]) right--;
+                            left++;
+                            right--;
+                            break;
+                        }
+                        case < 0:
+                            left++;
+                            break;
+                        default:
+                            right--;
+                            break;
+                    }
+                }
+            }
+
+            return triplets;
         }
     }
 }
